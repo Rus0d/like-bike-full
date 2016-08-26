dda6cec7041edda034503ec8076e225c61fa76f3
Rus0d <rusya.liamg@gmail.com>
2016-08-26 12:33:04 +0300

    likebike

---
+++ dist/scripts/init.js
@@ -0,0 +1,21 @@
+$(function() {
+    positionCalculator();
+});
+
+$(window).resize(function () {
+    positionCalculator();
+});
+
+function positionCalculator() {
+
+    var wrapperH = $('.wrapper').height(),
+        headerH = $('header').innerHeight(),
+        contentH = $('.content').innerHeight(),
+        diff = wrapperH - headerH - contentH;
+
+    if (diff > 0) {
+        $('.content').css('margin-top', diff*3/8 + 'px');
+        $('.like-bike-menu').css('margin-top', diff/8 + 'px');
+    }
+
+}
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+    renameCss = require('gulp-rename'),
+    uncss = require('gulp-uncss'),
+    notify = require('gulp-notify'),
+    minifyCss = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concatCss = require('gulp-concat-css'),
+    browserSync = require('browser-sync');
+
+gulp.task('default',['stylesCompiler', 'watch', 'browserSync']);
+
+gulp.task('stylesCompiler', function () {
+
+    gulp.src('styles/styleConstructor.css')
+        .pipe(concatCss("styles.css"))
+        .pipe(autoprefixer({ browsers: ["> 0%"] }))
+        //.pipe(uncss({
+        //    html: 'dist/*.html'
+        //}))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(notify('styles.css done!!!'));
+
+});
+
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: "dist"
+        },
+        port: 8080,
+        open: true,
+        notify: false
+    });
+});
+
+gulp.task('html', function () {
+    gulp.src('dist/*.html')
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['styles/*.css', 'dist/*.html'], ['stylesCompiler', 'html']);
+});
+
+// npm install --save-dev gulp-rename gulp-notify gulp-minify-css gulp-autoprefixer browser-sync gulp-concat-css gulp-uncss
+
