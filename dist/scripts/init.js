8a3a82389361a185c97228bb898c6e831841043c
Rus0d <rusya.liamg@gmail.com>
2016-09-05 10:46:58 +0300

    main slider without adaptive

--- dist/scripts/init.js
+++ dist/scripts/init.js
@@ -3,17 +3,3 @@ $(function() {
 
 $(window).resize(function () {
 });
-
-function positionCalculator() {
-
-    var wrapperH = $('.wrapper').height(),
-        headerH = $('header').innerHeight(),
-        contentH = $('.content').innerHeight(),
-        diff = wrapperH - headerH - contentH;
-
-    if (diff > 0) {
-        $('.content').css('margin-top', diff*3/8 + 'px');
-        $('.like-bike-menu').css('margin-top', diff/8 + 'px');
-    }
-
-}
