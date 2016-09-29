17e7e69e291f7ad520f642b973e6528bcf7968b1
Rus0d <rusya.liamg@gmail.com>
2016-09-29 15:36:00 +0300

    product page popup

--- dist/scripts/product-page-init.js
+++ dist/scripts/product-page-init.js
@@ -20,6 +20,9 @@ $(function() {
     $('.js-lazyYT').lazyYT('');
 
     descriptionTrigger();
+    testDrive();
+    maskedInput();
+    cityPicker();
 
 });
 
@@ -32,9 +35,37 @@ function descriptionTrigger() {
         });
         $(this).addClass('active');
 
-        $('.description-block .block-trigger li').css('display', 'none');
-        $('.description-block .block-trigger li').eq( $(this).index() ).css('display', 'block');
+        $('.description-block .block-trigger li').addClass("hide");
+        $('.description-block .block-trigger li').eq( $(this).index() ).removeClass("hide");
     });
 
 
+}
+
+function testDrive() {
+
+    $('#test-drive-btn').on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $("body").addClass("scroll");
+        $(".test-drive-form").removeClass("hide");
+    });
+
+    $(".popup, .close-btn").on("click", function(e) {
+        if( $(e.target).hasClass("popup") || $(e.target).hasClass("close-btn") ) {
+            $("body").removeClass("scroll");
+            $(".popup").addClass("hide");
+        }
+    });
+
+}
+
+function maskedInput(){
+    $('#phone').mask("+38(099) 999 99 99");
+}
+
+function cityPicker(){
+    $('.test-drive-form form #city').select2({
+        placeholder: "Выберите город"
+    });
 }
